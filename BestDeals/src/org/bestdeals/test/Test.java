9c148741656905a4366c0adfb518092ba46759ef
Ankit Kodalikar <Ankit Kodalikar@Ankit>
2016-05-07 20:29:37 +0530

    working demo(80%)

---
+++ BestDeals/src/org/bestdeals/test/Test.java
@@ -0,0 +1,17 @@
+package org.bestdeals.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+@Controller
+public class Test {
+
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public String test(){
+		return "index";
+	}
+	
+	
+	
+}
